7f857ded61d6d0959e292e7c0dd95d2b35787ae8
Kyle Mills <kyle.mills@uoit.net>
2021-11-22 15:43:11 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup( name='Plot1Qbit',
-       version='0.14',
+setup( name='PlotKM',
+       version='0.01',
        author='Kyle Mills',
        author_email="kyle.mills@uoit.net",
-       description="Set matplotlib style to conform with 1QBit's brand policies",
-       packages=['plot1qbit'],
+       description="Set matplotlib style to conform with Kyle Mills' style",       packages=['plotkm'],
        install_requires=['matplotlib>=3.1.0'],
        include_package_data=True
     )
